a70bc426602179f2561ca2ade4ee8685a7b44254
habvi <a2231250601h@yahoo.co.jp>
2022-05-30 02:17:09 +0900

    add struct

--- 011_SlidingTreePuzzle/sliding-tree-puzzle/src/main.rs
+++ 011_SlidingTreePuzzle/sliding-tree-puzzle/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::stdin;
 use std::str::FromStr;
+use std::fmt;
 
 fn input_t<T: FromStr>() -> T {
     let mut s: String = String::new();
@@ -24,18 +25,36 @@ fn eprint_tiles(tiles: &mut Vec<Vec<usize>>) -> () {
     eprintln!("----")
 }
 
-fn move_test(ans: &mut Vec<&str>) -> () {
-    ans.push("U");
-    ans.push("D");
-    ans.push("R");
-    ans.push("L");
+struct Info {
+    ans: Vec<String>,
+    tiles: Vec<Vec<usize>>,
+    tile_num: Vec<usize>,
+    gy: usize,
+    gx: usize,
+}
+
+impl fmt::Display for Info {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "gy, gx : ({}, {})\ntiles : {:?}\n-----",
+        self.gx, self.gy, self.tiles)
+    }
+}
+
+fn move_1(g: &mut Info) -> () {
+    // example
+    g.ans.push("UUUU".to_string());
+    let tmp1: usize = g.tiles[0][0];
+    let tmp2: usize = g.tiles[1][0];
+    g.tiles[1][0] = tmp1;
+    g.tiles[0][0] = tmp2;
+    g.tile_num[0] += 0;
+    g.gx += 1;
 }
 
 fn main() {
     let nt: Vec<usize> = input_vec();
     let n: usize = nt[0];
     // max times : 2 * n^3
-    #[allow(unused_variables)]
     let t: usize = nt[1];
 
     let mut tiles: Vec<Vec<usize>> = Vec::new();
@@ -67,16 +86,29 @@ fn main() {
     }
     assert_eq!(0, tiles[gy][gx]);
     eprint_tiles(&mut tiles);
-    eprintln!("start (y, x) : {}, {}", gy, gx);
     eprintln!("tile num : {:?}", tile_num);
+    eprintln!("-----");
 
-    let mut ans: Vec<&str> = Vec::new();
+    let ans: Vec<String> = Vec::new();
+    let mut g: Info = Info {
+        ans: ans,
+        tiles: tiles,
+        tile_num: tile_num,
+        gy: gy,
+        gx: gx,
+    };
+
+    eprintln!("{}", g);
 
     // move
-    move_test(&mut ans);
+    move_1(&mut g);
+    eprintln!("{}", g);
 
     // output
-    let output: String = ans.iter().map(|c| c.trim()).collect::<Vec<_>>().join("");
+    while g.ans.len() > t {
+        g.ans.pop();
+    }
+    let output: String = g.ans.iter().map(|c| c.trim()).collect::<Vec<_>>().join("");
     eprintln!("{}", output);
     println!("{}", output)
 }
